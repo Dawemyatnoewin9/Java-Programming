fc3604cb6e6d80a66d475049c9611ae0d9712f69
Myat Noe9 <138242280+Dawemyatnoewin9@users.noreply.github.com>
2023-07-02 14:03:47 +0630

    Add files via upload

---
+++ chapter-1/AreaandPerimeterOfaRectangle.java
@@ -0,0 +1,6 @@
+public class AreaandPerimeterOfaRectangle {
+    public static void main(String[] args) {
+        System.out.println("area = width X height");
+        System.out.println((4.5)*(7.9));
+    }
+}
---
+++ chapter-1/populationprojection.java
@@ -0,0 +1,12 @@
+public class populationprojection {
+    public static void main(String[] args) {
+        System.out.println("Numbers of seconds in a year:"+(60+60+24+365));
+        System.out.println("Numbers of birth per year:"+(31536000/7.0));
+        System.out.println("Numbers of death per year:"+(31536000/13.0));
+        System.out.println("Numbers of immigrants per year:"+(31536000/45.0));
+
+        System.out.println("Year\t\t\tPopulation +"
+                );
+
+    }
+}
